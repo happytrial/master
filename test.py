19e956ae6ffcb122fd401b95e3ff06b7b7325313
user <user@TEST.myguest.virtualbox.org>
2023-09-18 10:23:20 +0530

    new files

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 digits = open("ints.txt").readlines()
 d = {}
 for x in range(len(digits)):
@@ -7,7 +8,11 @@ D_keys = list(d.keys())
 D_values = list(d.values())
 
 result = open("sum.txt").readlines()
+time1 = open("time.txt").readlines()
 
 class TestCalc(unittest.TestCase):
     def test_addition(self):
         self.assertEqual((D_values[0]+D_values[1]), int(result[0]))
+
+    def test_time(self):
+        self.assertEqual(time.ctime(time.time()), str(time1[0]))
