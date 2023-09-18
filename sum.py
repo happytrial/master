de6029a2fb8f2274c00cdb54178ce8f620b81dee
user <user@TEST.myguest.virtualbox.org>
2023-09-18 11:20:16 +0530

    added a space to test commit

--- sum.py
+++ sum.py
@@ -8,7 +8,6 @@ D_values = list(d.values())
 result = str((D_values[0]-D_values[1]))
 print("sum of "+ str(D_values[0]) +" and " + str(D_values[1]) +" is: " + result)
 
-
 with open('sum.txt', 'a') as f:
     f.seek(0)
     f.truncate()
