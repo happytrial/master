a869761b1c8075aa547038d7249d8b972a5db56b
user <user@TEST.myguest.virtualbox.org>
2023-09-18 10:06:20 +0530

    new files

--- sum.py
+++ sum.py
@@ -1,3 +1,14 @@
-print("sum of 1 and 2 is: ")
+digits = open("ints.txt").readlines()
+d = {}
+for x in range(len(digits)):
+    d["int{0}".format(x)] = int(digits[x].strip())
+D_keys = list(d.keys())
+D_values = list(d.values())
+#print(D_keys[0])
+print("sum of "+ str(D_values[0]) +" and " + str(D_values[1]) +" is: " + str((D_values[0]+D_values[1])))
 
-print(1+2)
+result = str((D_values[0]+D_values[1]))
+with open('sum.txt', 'a') as f:
+    f.seek(0)
+    f.truncate()
+    f.write(result)
---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+digits = open("ints.txt").readlines()
+d = {}
+for x in range(len(digits)):
+    d["int{0}".format(x)] = int(digits[x].strip())
+D_keys = list(d.keys())
+D_values = list(d.values())
+
+result = open("sum.txt").readlines()
+
+class TestCalc(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual((D_values[0]+D_values[1]), int(result[0]))
