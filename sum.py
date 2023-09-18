f361ad36efda25cbd766a580bdffffadbcfe69ad
user <user@TEST.myguest.virtualbox.org>
2023-09-18 11:51:31 +0530

    commit to test pipelines

--- sum.py
+++ sum.py
@@ -8,6 +8,7 @@ D_values = list(d.values())
 result = str((D_values[0]+D_values[1]))
 print("sum of "+ str(D_values[0]) +" and " + str(D_values[1]) +" is: " + result)
 
+
 with open('sum.txt', 'a') as f:
     f.seek(0)
     f.truncate()
