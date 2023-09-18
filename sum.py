4561d6516e700350a34ff39c4b912919f6075a0f
user <user@TEST.myguest.virtualbox.org>
2023-09-18 11:08:37 +0530

    changes to sum.py

--- sum.py
+++ sum.py
@@ -5,9 +5,10 @@ for x in range(len(digits)):
 D_keys = list(d.keys())
 D_values = list(d.values())
 
-print("sum of "+ str(D_values[0]) +" and " + str(D_values[1]) +" is: " + str((D_values[0]+D_values[1])))
-
 result = str((D_values[0]-D_values[1]))
+print("sum of "+ str(D_values[0]) +" and " + str(D_values[1]) +" is: " + result)
+
+
 with open('sum.txt', 'a') as f:
     f.seek(0)
     f.truncate()
